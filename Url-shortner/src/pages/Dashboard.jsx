4f37ec385aceed786e15395b88edcf5ac79a889d
Bhavishya Tomer <bhavishya.tomer@dxc.com>
2024-07-02 15:32:03 +0530

    dashboard

--- Url-shortner/src/pages/Dashboard.jsx
+++ Url-shortner/src/pages/Dashboard.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='flex flex-col item-center'>
+      <h2 className='my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-white text-center font-extrabold'>
+        Change your Boring Urls here.😎
+      </h2>
+      <div className='sm:h-14 flex flex-col sm:flex flex-row w-full md:w-2/4 gap-2'>
+      
+      </div>
+    </div>
   )
 }
 
